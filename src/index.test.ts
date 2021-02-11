07acf6f4e5f982f8d630e948dc4858fda11616aa
Patrick Smith <pgwsmith@gmail.com>
2021-02-11 11:45:43 +0000

    Get compound states working!

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,13 @@
-import { always, call, cond, entry, exit, on, start } from "./index";
+import { always, compound, cond, entry, exit, on, start } from "./index";
 
 const fetch = jest.fn();
 beforeEach(fetch.mockClear);
 
 const finishedLoading = jest.fn();
 beforeEach(finishedLoading.mockClear);
+
+const succeeded = jest.fn();
+beforeEach(succeeded.mockClear);
 
 describe("Machine with entry and exit actions", () => {
   const someURL = new URL("https://example.org/");
@@ -22,7 +25,9 @@ describe("Machine with entry and exit actions", () => {
       yield on("SUCCESS", success);
       yield on("FAILURE", failure);
     }
-    function* success() {}
+    function* success() {
+      yield entry(succeeded);
+    }
     function* failure() {
       yield on("RETRY", loading);
     }
@@ -63,11 +68,13 @@ describe("Machine with entry and exit actions", () => {
       expect(finishedLoading).toHaveBeenCalledTimes(1);
       expect(loader.changeCount).toEqual(2);
       expect(loader.value).toEqual("success");
+      expect(succeeded).toHaveBeenCalledTimes(1);
 
       const transitionResult2 = loader.next("FETCH");
-      expect(transitionResult2.actions).toEqual([]);
+      // expect(transitionResult2.actions).toEqual([]);
       expect(loader.changeCount).toEqual(2);
       expect(loader.value).toEqual("success");
+      expect(succeeded).toHaveBeenCalledTimes(1);
 
       await loader.resolved;
     });
@@ -79,7 +86,7 @@ describe("Machine with entry and exit actions", () => {
     });
 
     test("sending events", async () => {
-      const loader = start(Loader, [{ url: someURL }]);
+      const loader = start(Loader);
       expect(loader.value).toEqual("idle");
 
       const transitionResult = loader.next("FETCH");
@@ -116,9 +123,7 @@ describe("Machine with entry and exit actions", () => {
   });
 });
 
-describe("Form Field Machine with entry and exit actions", () => {
-  // const validate = jest.fn();
-  // beforeEach(validate.mockClear);
+describe("Form Field Machine with always()", () => {
   const isValid = jest.fn();
   beforeEach(isValid.mockClear);
 
@@ -127,20 +132,12 @@ describe("Form Field Machine with entry and exit actions", () => {
       yield on("CHANGE", editing);
     }
     function* editing() {
-      // yield exit(validate);
       yield on("CHANGE", editing);
       yield on("BLUR", validating);
     }
     function* validating() {
       yield always(cond(isValid, valid));
       yield always(invalid);
-
-      // yield on(null, cond(isValid, valid));
-      // yield on(null, invalid);
-
-      // yield always([cond(isValid, valid), invalid]);
-      // return [cond(isValid, valid), invalid];
-      // return conds([[isValid, valid], [true, invalid]]);
     }
     function* invalid() {
       yield on("CHANGE", editing);
@@ -151,11 +148,6 @@ describe("Form Field Machine with entry and exit actions", () => {
 
     return initial;
   }
-
-  test("creating", () => {
-    const formField = start(FormField);
-    expect(formField).toBeDefined();
-  });
 
   describe("when is valid", () => {
     beforeEach(() => {
@@ -173,11 +165,11 @@ describe("Form Field Machine with entry and exit actions", () => {
 
       formField.next("CHANGE");
       expect(formField.value).toEqual("editing");
-      expect(formField.changeCount).toEqual(2);
+      expect(formField.changeCount).toEqual(1);
 
       formField.next("BLUR");
       expect(formField.value).toEqual("valid");
-      expect(formField.changeCount).toEqual(4);
+      expect(formField.changeCount).toEqual(3);
     });
   });
 
@@ -197,106 +189,79 @@ describe("Form Field Machine with entry and exit actions", () => {
 
       formField.next("CHANGE");
       expect(formField.value).toEqual("editing");
-      expect(formField.changeCount).toEqual(2);
+      expect(formField.changeCount).toEqual(1);
 
       formField.next("BLUR");
       expect(formField.value).toEqual("invalid");
-      expect(formField.changeCount).toEqual(4);
+      expect(formField.changeCount).toEqual(3);
     });
   });
 });
 
-describe("Machine with call", () => {
-  function Loader({ url }: { url: URL }) {
-    function* idle() {
-      yield on("FETCH", loading);
+describe("Hierarchical Traffic Lights Machine", () => {
+  // const validate = jest.fn();
+  // beforeEach(validate.mockClear);
+  const isValid = jest.fn();
+  beforeEach(isValid.mockClear);
+
+  function PedestrianFactory() {
+    function* walk() {
+      yield on("PED_COUNTDOWN", wait);
     }
-    function* loading() {
-      yield call(fetch, [url.toString()]);
-      yield on("SUCCESS", success);
-      yield on("FAILURE", failure);
+    function* wait() {
+      yield on("PED_COUNTDOWN", stop);
     }
-    function* success() {}
-    function* failure() {
-      yield on("RETRY", loading);
+    function* stop() {}
+    function* blinking() {}
+
+    return { walk, blinking };
+  }
+  function* TrafficLights() {
+    const { walk, blinking } = PedestrianFactory();
+
+    function* green() {
+      yield on("TIMER", yellow);
+    }
+    function* yellow() {
+      yield on("TIMER", red);
+    }
+    function* red() {
+      yield on("TIMER", green);
+
+      return walk;
     }
 
-    return idle;
+    yield on("POWER_OUTAGE", compound(red, blinking));
+    yield on("POWER_RESTORED", compound(red));
+
+    return green;
   }
 
-  const someURL = new URL("https://example.org/");
+  test("sending events", () => {
+    const machine = start(TrafficLights);
+    expect(machine).toBeDefined();
+    expect(machine.value).toEqual("green");
 
-  test("creating", () => {
-    const loader = start(Loader, [{ url: someURL }]);
-    expect(loader).toBeDefined();
-  });
+    machine.next("TIMER");
+    expect(machine.value).toEqual("yellow");
+    expect(machine.changeCount).toEqual(1);
 
-  describe("when fetch succeeds", () => {
-    beforeEach(() => {
-      fetch.mockResolvedValue(42);
-    });
+    machine.next("TIMER");
+    // expect(machine.value).toEqual("red");
+    // expect(machine.value).toEqual(["red", "walk"]);
+    expect(machine.value).toEqual({ "red": "walk" });
+    expect(machine.changeCount).toEqual(3);
 
-    test("sending events", async () => {
-      const loader = start(Loader, [{ url: someURL }]);
-      expect(loader.value).toEqual("idle");
-      await expect(loader.resolved).resolves.toEqual([]);
-
-      loader.next("NOOP");
-      expect(loader.value).toEqual("idle");
-      expect(loader.changeCount).toEqual(0);
-      await expect(loader.resolved).resolves.toEqual([]);
-
-      loader.next("FETCH");
-      expect(loader.value).toEqual("loading");
-      expect(loader.changeCount).toEqual(1);
-
-      expect(fetch).toHaveBeenCalledWith("https://example.org/");
-
-      await expect(loader.resolved).resolves.toEqual([42]);
-      expect(loader.changeCount).toEqual(2);
-      expect(loader.value).toEqual("success");
-
-      loader.next("FETCH");
-      expect(loader.changeCount).toEqual(2);
-
-      await loader.resolved;
-    });
-  });
-
-  describe("when fetch fails", () => {
-    beforeEach(() => {
-      fetch.mockRejectedValueOnce(new Error("Failed!")).mockResolvedValue(42);
-    });
-
-    test("sending events", async () => {
-      const loader = start(Loader, [{ url: someURL }]);
-      expect(loader.value).toEqual("idle");
-
-      loader.next("FETCH");
-      expect(loader.value).toEqual("loading");
-      expect(loader.changeCount).toEqual(1);
-
-      expect(fetch).toHaveBeenCalledTimes(1);
-      expect(fetch).toHaveBeenLastCalledWith("https://example.org/");
-
-      await expect(loader.resolved).rejects.toEqual(new Error("Failed!"));
-      expect(loader.changeCount).toEqual(2);
-      expect(loader.value).toEqual("failure");
-
-      loader.next("FETCH");
-      expect(fetch).toHaveBeenCalledTimes(1);
-      expect(loader.changeCount).toEqual(2);
-
-      loader.next("RETRY");
-      expect(loader.value).toEqual("loading");
-      expect(loader.changeCount).toEqual(3);
-
-      expect(fetch).toHaveBeenCalledTimes(2);
-      expect(fetch).toHaveBeenLastCalledWith("https://example.org/");
-
-      await expect(loader.resolved).resolves.toEqual([42]);
-      expect(loader.changeCount).toEqual(4);
-      expect(loader.value).toEqual("success");
-    });
+    machine.next("TIMER");
+    expect(machine.value).toEqual("green");
+    expect(machine.changeCount).toEqual(4);
+    
+    machine.next("POWER_RESTORED");
+    expect(machine.value).toEqual({ "red": "walk" });
+    expect(machine.changeCount).toEqual(6);
+    
+    machine.next("POWER_OUTAGE");
+    expect(machine.value).toEqual({ "red": "blinking" });
+    expect(machine.changeCount).toEqual(7);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,11 @@ export interface Cond {
   cond: Function;
   target: StateDefinition;
 }
-export type Target = StateDefinition | Cond;
+export interface Compound {
+  type: "compound";
+  targets: Array<StateDefinition>;
+}
+export type Target = StateDefinition | Cond | Compound;
 export interface On {
   type: "on";
   on: string;
@@ -63,137 +67,319 @@ export function cond(cond: () => boolean, target: StateDefinition): Cond {
   return { type: "cond", cond, target };
 }
 
-export interface MachineInstance extends Iterator<string, void, string> {
+export function compound(...targets: Array<StateDefinition>): Compound {
+  return { type: "compound", targets };
+}
+
+export interface MachineInstance extends Iterator<null | string | Record<string, string>, void, string> {
   changeCount: number;
-  value: string;
+  value: null | string | Record<string, string>;
   resolved: null | Promise<Array<any>>;
   done: boolean;
   next(
     ...args: [string]
-  ): IteratorResult<string, void> &
+  ): IteratorResult<null | string | Record<string, string>> &
     PromiseLike<any> & {
       actions: Array<EntryAction | ExitAction>;
     };
 }
 
-export function start<Arguments extends Array<any>>(
-  machine: (...args: Arguments) => () => Generator<Yielded>,
-  args?: Arguments
-): MachineInstance {
-  const initialGenerator = machine.apply(null, args);
+class Handlers {
+  private eventsMap = new Map<string, Target>();
+  private alwaysArray = new Array<Target>();
+  private entryActions = [] as Array<EntryAction>;
+  private exitActions = [] as Array<ExitAction>;
+  private promises = [] as Array<Promise<any>>;
+  private promise: null | Promise<Array<any>> = null;
+  
+  *actions(): Generator<EntryAction, void, undefined> {
+    yield* this.entryActions;
+  }
+  
+  reset() {
+    this.eventsMap.clear();
+    this.entryActions.splice(0, Infinity);
+    this.exitActions.splice(0, Infinity);
+    this.alwaysArray.splice(0, Infinity);
+    this.promises.splice(0, Infinity);
+  }
+  
+  add(value: Yielded) {
+    if (value.type === "entry") {
+      this.entryActions.push(value);
+      
+      const result = new Promise((resolve) => {
+        resolve(value.f());
+      });
+      this.promises.push(result);
+      
+    } else if (value.type === "exit") {
+      this.exitActions.push(value);
+    } else if (value.type === "on") {
+      this.eventsMap.set(value.on, value.target);
+    } else if (value.type === "always") {
+      this.alwaysArray.push(value.target);
+    }
+  }
+  
+  finish(): null | Promise<Array<any>> {
+    if (this.promises.length === 0) return null;
+    
+    const promise = Promise.resolve(this.promise)
+      .catch(() => {})
+      .then(() => Promise.all(this.promises));
+    
+    this.promise = promise;
+    
+    return promise;
+  }
+  
+  runExit() {
+    this.exitActions.forEach((action) => {
+      action.f();
+    });
+  }
+  
+  runAlways(process: (target: Target) => boolean) {
+    this.alwaysArray.some(process);
+  }
+  
+  targetForEvent(event: string) {
+    return this.eventsMap.get(event);
+  }
+}
 
-  const eventsMap: Map<string, Target> = new Map();
-  const alwaysArray: Array<Target> = [];
-
-  let state = {
-    changeCount: -1,
-    current: "",
-    entryActions: [] as Array<EntryAction>,
-    exitActions: [] as Array<ExitAction>,
-    resolved: null as Promise<Array<any>> | null,
-  };
-
-  function processTarget(target: Target): boolean {
+class InternalInstance {
+  private definition: (() => StateDefinition) | (() => Generator<Yielded, StateDefinition, never>)
+  private parent: null | InternalInstance
+  private globalHandlers = new Handlers()
+  private resolved = null as Promise<Array<any>> | null
+  // TODO: Just can have reference to child instance, and use childInstance.definition
+  child: null | { definition: () => Generator<Yielded, StateDefinition, never>, instance: InternalInstance } = null
+  
+  constructor(
+    parent: null | InternalInstance,
+    machineDefinition: (() => StateDefinition) | (() => Generator<Yielded, StateDefinition, never>),
+    private callbacks: {
+      changeCount: number;
+      didChange: () => void;
+      sendEvent: (event: string, changeCount?: number) => void;
+    }
+  ) {
+    this.definition = machineDefinition;
+    this.parent = parent;
+    this.consume(machineDefinition);
+  }
+  
+  get current(): null | string | Record<string, any> {
+    if (this.child === null) {
+      return this.definition.name;
+    } else {
+      return { [this.definition.name]: this.child.instance.current };
+    }
+    // if (this.child === null) {
+    //   return null;
+    // }
+    // 
+    // return this.child.definition.name;
+  }
+  
+  private *generateActions(): Generator<EntryAction, void, undefined> {
+    yield* this.globalHandlers.actions();
+    if (this.child !== null) {
+      yield* this.child.instance.generateActions();
+    }
+  }
+  
+  get actions(): Array<EntryAction> {
+    return Array.from(this.generateActions());
+  }
+  
+  private async *valuePromises(): AsyncGenerator<any, void, undefined> {
+    if (this.resolved !== null) {
+      yield* await this.resolved;
+    }
+    if (this.child !== null) {
+      yield* this.child.instance.valuePromises();
+    }
+  }
+  
+  get value(): Promise<Array<any>> {
+    const build = async () => {
+      const values: Array<any> = [];
+      for await (const value of this.valuePromises()) {
+        values.push(value);
+      }
+      return values;
+    }
+    
+    return build().then(values => Promise.all(values));
+  }
+  
+  consume(stateGenerator: (() => StateDefinition) | (() => Generator<Yielded, StateDefinition, never>)) {
+    const initialReturn = stateGenerator();
+    
+    this.willEnter();
+    
+    this.globalHandlers.reset();
+    
+    if (initialReturn[Symbol.iterator]) {
+      const iterator = initialReturn[Symbol.iterator]();
+      while (true) {
+        const item = iterator.next()
+        if (item.done) {
+          var initialGenerator = item.value;
+          break;
+        }
+        
+        this.globalHandlers.add(item.value);
+      }
+      
+      const promise = this.globalHandlers.finish();
+      this.resolved = promise;
+  
+      if (promise !== null) {
+        const snapshotCount = this.callbacks.changeCount;
+        promise
+          .then(() => {
+            this.callbacks.sendEvent("SUCCESS", snapshotCount);
+          })
+          .catch(() => {
+            this.callbacks.sendEvent("FAILURE", snapshotCount);
+          });
+      }
+      
+    } else if (typeof initialReturn === 'function') {
+      var initialGenerator = initialReturn as any;
+    } else {
+      // throw new Error(`State Machine definition returned invalid initial value ${initialReturn}`);
+    }
+    
+    this.transitionTo(initialGenerator);
+  }
+  
+  willEnter() {
+    this.callbacks.didChange();
+  }
+  
+  didEnter() {
+    this.globalHandlers.runAlways(target => this.processTarget(target));
+  }
+  
+  willExit() {
+    this.globalHandlers.runExit();
+  }
+  
+  transitionTo(stateDefinition?: StateDefinition) {
+    if (stateDefinition !== undefined) {
+      if (this.child !== null) {
+        if (this.child.definition === stateDefinition) {
+          return;
+        }
+        
+        this.child.instance.willExit();  
+      }
+      
+      this.willExit();
+  
+      // this.resolved = null;
+      
+      const childInstance = new InternalInstance(this, stateDefinition, this.callbacks);
+      this.child = { definition: stateDefinition, instance: childInstance };
+      childInstance.didEnter();
+    }
+    
+    // this.didEnter();
+  }
+  
+  processTarget(target: Target): boolean {
     if ('type' in target) {
-      const result = target.cond();
-      if (result) {
-        transitionTo(target.target);
-        return true;
+      if (target.type === "cond") {
+        const result = target.cond();
+        if (result) {
+          if (this.parent !== null) {
+            this.parent.transitionTo(target.target);
+            return true;
+          }
+        }
+      } else if (target.type === "compound") {
+        let receiver: InternalInstance = this;
+        for (const nestedTarget of target.targets) {
+          receiver.transitionTo(nestedTarget);
+          if (receiver.child !== null) {
+            receiver = receiver.child.instance;
+          } else {
+            break;
+          }
+        }
+        // const targetA = target.targets[0];
+        // this.transitionTo(targetA);
       }
     } else {
-      transitionTo(target);
-      return true;
+      if (this.parent !== null) {
+        this.parent.transitionTo(target);
+        return true;
+      }
     }
 
     return false;
   }
 
-  function receive(event: string, count: number) {
-    if (count !== state.changeCount) return;
+  receive(event: string) {
+    this.child?.instance.receive(event);
 
-    const target = eventsMap.get(event);
-    if (target) {
-      processTarget(target);
+    const target = this.globalHandlers.targetForEvent(event);
+    if (target !== undefined) {
+      this.processTarget(target);
     }
   }
+}
 
-  function transitionTo(stateGenerator: () => Generator<Yielded>) {
-    state.exitActions.forEach((action) => {
-      action.f();
-    });
-
-    state.changeCount++;
-    state.current = stateGenerator.name;
-    state.entryActions.splice(0, Infinity);
-    state.exitActions.splice(0, Infinity);
-    state.resolved = null;
-    eventsMap.clear();
-    alwaysArray.splice(0, Infinity);
-
-    const results: Array<any> = [];
-
-    const iterable = stateGenerator();
-    for (const value of iterable) {
-      if (value.type === "entry") {
-        state.entryActions.push(value);
-        // const result = Promise.resolve(value);
-        const result = new Promise((resolve) => {
-          resolve(value.f());
-        });
-        results.push(result);
-      } else if (value.type === "exit") {
-        state.exitActions.push(value);
-      } else if (value.type === "call") {
-        const result = new Promise((resolve) =>
-          resolve(value.f.apply(null, value.args))
-        );
-        results.push(result);
-      } else if (value.type === "on") {
-        eventsMap.set(value.on, value.target);
-      } else if (value.type === "always") {
-        alwaysArray.push(value.target);
+export function start(
+  machine: (() => StateDefinition) | (() => Generator<Yielded, StateDefinition, never>)
+): MachineInstance {
+  let changeCount = -1;
+  
+  const rootName = machine.name;
+  const instance: InternalInstance = new InternalInstance(
+    null,
+    machine,
+    {
+      get changeCount() { return changeCount },
+      didChange() { changeCount += 1 },
+      sendEvent(event, snapshotCount) {
+        if (typeof snapshotCount === "number" && snapshotCount !== changeCount) {
+          return;
+        }
+        instance.receive(event);
       }
     }
-
-    // const promise = Promise.all(results);
-    const promise = Promise.resolve(state.resolved)
-      .catch(() => {})
-      .then(() => Promise.all(results));
-    state.resolved = promise;
-
-    const snapshotCount = state.changeCount;
-    promise
-      .then(() => receive("SUCCESS", snapshotCount))
-      .catch(() => receive("FAILURE", snapshotCount));
-    
-    alwaysArray.some(processTarget);
-  }
-
-  transitionTo(initialGenerator);
+  );
+  
+  changeCount = 0;
 
   return {
     get changeCount() {
-      return state.changeCount;
+      return changeCount;
     },
     get value() {
-      return state.current;
+      return instance.current !== null ? instance.current[rootName] : null;
     },
     get resolved() {
-      return state.resolved;
+      return instance.value;
     },
     next(event: string) {
-      receive(event, state.changeCount);
-      const promise = state.resolved!;
-      Promise.resolve;
+      instance.receive(event);
+      const promise = instance.value;
       return {
-        value: state.current,
-        actions: Array.from(state.entryActions),
-        then: promise.then.bind(promise),
+        value: instance.current !== null ? instance.current[rootName] : null,
+        actions: instance.actions,
+        then: promise?.then.bind(promise),
         done: false,
       };
     },
     get done() {
-      return false;
+      return instance.child !== null;
     },
   };
 }
